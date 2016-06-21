630ae288c06411ebf7c97aa98db6eb79169cfb01
Nicolas Meier <trehex@gmail.com>
2016-06-21 09:22:15 +0200

    added Semester, Fach and Note class

---
+++ Notenverwaltung/app/src/main/java/com/factbz/notenverwaltung/Model/Fach.java
@@ -0,0 +1,16 @@
+package com.factbz.notenverwaltung.Model;
+
+/**
+ * Created by Nicolas on 21.06.2016.
+ */
+public class Fach {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
