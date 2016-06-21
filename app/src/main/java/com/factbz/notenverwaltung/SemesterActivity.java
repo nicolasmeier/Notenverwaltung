1ee2d5ee060194fada51956a9aed450f69a7032a
Nicolas Meier <trehex@gmail.com>
2016-06-21 11:16:55 +0200

    Created new Project
    ..

---
+++ app/src/main/java/com/factbz/notenverwaltung/SemesterActivity.java
@@ -0,0 +1,13 @@
+package com.factbz.notenverwaltung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SemesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semester);
+    }
+}
